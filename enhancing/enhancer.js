44ed2a92e62d4987b916a9c81afb696227a212a6
mikecrbenton <mikecrbenton@hotmail.com>
2020-12-22 20:41:58 -0600

    mvp and 1 stretch

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -4,19 +4,62 @@ module.exports = {
   repair,
   get,
 };
+/*
+const item = {
+   name: "",
+   durability: ,   // number 0 to 100
+   enhancement:    // number 0 to 20
+}
+ */
 
 function success(item) {
-  return { ...item };
+   if(item.enhancement === 20){
+      return { ...item };
+   }else if(item.enhancement < 20){ 
+      return { ...item, enhancement: item.enhancement + 1}
+   }
 }
 
 function fail(item) {
-  return { ...item };
-}
+   // UNDER 15----------------
+   if( item.enhancement < 15 ){
+
+      if( item.durability <= 5 )
+         return { ...item, durability: 0 };
+      else{
+         return { ...item, durability: item.durability - 5 };
+      }
+   // EQUALS 15----------------
+   }else if( item.enhancement === 15 ){ 
+      
+      if( item.durability <= 5 )
+         return { ...item, durability: 0 };
+      else{
+         return { ...item, durability: item.durability - 10 };
+      }
+   // ABOVE 15------------------
+   }else if( item.enhancement > 15 ){
+
+      if( item.durability <= 5 )
+         return { ...item, 
+            durability: 0, 
+            enhancement: (item.enhancement - 1) }
+      else{
+         return { ...item, 
+            durability: (item.durability - 10), 
+            enhancement: (item.enhancement - 1) }
+      }
+   }
+} // end fail()
 
 function repair(item) {
-  return { ...item };
+  return { ...item, durability : 100 };
 }
 
 function get(item) {
-  return { ...item };
+   if( item.enhancement === 0 ){ 
+      return { ...item };
+   }else if( item.enhancement > 0 ){
+      return { ...item, name: `[+${item.enhancement}] ${item.name}` }
+   }
 }
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,127 @@
 const enhancer = require('./enhancer.js');
 // test away!
+
+describe("testing enhancer.js", ()=> {
+
+
+   test("repair method", ()=> {
+      expect( enhancer.repair({
+         name: "Mike Benton",
+         durability: 20,
+         enhancement: 20
+      })).toEqual({
+         name: "Mike Benton",
+         durability: 100,
+         enhancement: 20
+      })
+   })
+
+   test("success method", ()=> {
+      expect( enhancer.success({
+         name: "Mike Benton",
+         durability: 20,
+         enhancement: 18
+      })).toEqual({
+         name: "Mike Benton",
+         durability: 20,
+         enhancement: 19
+      })
+   })
+
+   test("fail method - enhancement under 15, durability under 5", ()=> {
+      expect( enhancer.fail({
+         name: "Mike Benton",
+         durability: 4,
+         enhancement: 12
+      })).toEqual({
+         name: "Mike Benton",
+         durability: 0,
+         enhancement: 12
+      })
+   })
+
+   test("fail method - enhancement under 15, durability over 5", ()=> {
+      expect( enhancer.fail({
+         name: "Mike Benton",
+         durability: 23,
+         enhancement: 12
+      })).toEqual({
+         name: "Mike Benton",
+         durability: 18,
+         enhancement: 12
+      })
+   })
+
+   test("fail method - enhancement equals 15, durability under 5", ()=> {
+      expect( enhancer.fail({
+         name: "Mike Benton",
+         durability: 3,
+         enhancement: 15
+      })).toEqual({
+         name: "Mike Benton",
+         durability: 0,
+         enhancement: 15
+      })
+   })
+
+   test("fail method - enhancement equals 15, durability over 5", ()=> {
+      expect( enhancer.fail({
+         name: "Mike Benton",
+         durability: 35,
+         enhancement: 15
+      })).toEqual({
+         name: "Mike Benton",
+         durability: 25,
+         enhancement: 15
+      })
+   })
+
+   test("fail method - enhancement over 15, durability under 5", ()=> {
+      expect( enhancer.fail({
+         name: "Mike Benton",
+         durability: 3,
+         enhancement: 16
+      })).toEqual({
+         name: "Mike Benton",
+         durability: 0,
+         enhancement: 15
+      })
+   })
+
+   test("fail method - enhancement over 15, durability over 5", ()=> {
+      expect( enhancer.fail({
+         name: "Mike Benton",
+         durability: 30,
+         enhancement: 25
+      })).toEqual({
+         name: "Mike Benton",
+         durability: 20,
+         enhancement: 24
+      })
+   })
+
+   test("get method - enhancement over 0 ", ()=> {
+      expect( enhancer.get({
+         name: "Mike Benton",
+         durability: 30,
+         enhancement: 25
+      })).toEqual({
+         name: "[+25] Mike Benton",
+         durability: 30,
+         enhancement: 25
+      })
+   })
+
+   test("get method - enhancement equals 0 ", ()=> {
+      expect( enhancer.get({
+         name: "Mike Benton",
+         durability: 30,
+         enhancement: 0
+      })).toEqual({
+         name: "Mike Benton",
+         durability: 30,
+         enhancement: 0
+      })
+   })
+
+}) // describe
